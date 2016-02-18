877f6dacd6fb8b209d500f03471534bd034b6422
JHuppi <jhuppi@hotmail.com>
2016-02-18 16:01:40 -0700

    add new code for mentor session

--- script.js
+++ script.js
@@ -10,28 +10,28 @@ var progress = document.getElementById("progress");
 var bufferBar = document.getElementById('buffer');
 var progressBar = document.getElementById("time-bar");
 
-var cap1 = document.getElementById("cap1");
-var cap2 = document.getElementById("cap2");
-var cap3 = document.getElementById("cap3");
-var cap4 = document.getElementById("cap4");
-var cap5 = document.getElementById("cap5");
-var cap6 = document.getElementById("cap6");
-var cap7 = document.getElementById("cap7");
-var cap8 = document.getElementById("cap8");
-var cap9 = document.getElementById("cap9");
-var cap10 = document.getElementById("cap10");
-var cap11 = document.getElementById("cap11");
-var cap12 = document.getElementById("cap12");
-var cap13 = document.getElementById("cap13");
-var cap14 = document.getElementById("cap14");
-var cap15 = document.getElementById("cap15");
-var cap16 = document.getElementById("cap16");
+var cap = [document.getElementById("cap1"),
+            document.getElementById("cap2"),
+            document.getElementById("cap3"),
+            document.getElementById("cap4"),
+            document.getElementById("cap5"),
+            document.getElementById("cap6"),
+            document.getElementById("cap7"),
+            document.getElementById("cap8"),
+            document.getElementById("cap9"),
+            document.getElementById("cap10"),
+            document.getElementById("cap11"),
+            document.getElementById("cap12"),
+            document.getElementById("cap13"),
+            document.getElementById("cap14"),
+            document.getElementById("cap15"),
+            document.getElementById("cap16")];
 
 video.controls = false;
 
 //Play-Pause Button
 playButton.addEventListener("click", function() {
-    if (video.paused === true) {
+    if (video.paused) {
         video.play();
         playButton.innerHTML = '<img src="icon/pause-icon.png" alt="pause">';
     } else {
@@ -51,14 +51,32 @@ muteButton.addEventListener("click", function() {
     }
 });
 
+function isFullScreen() {
+    return !!(document.fullScreen || 
+              document.webkitIsFullScreen || 
+              document.mozFullScreen ||
+              document.msFullscreenElement ||
+              document.fullscreenElement);
+}
+
 //FullScreen Button
 fullScreenButton.addEventListener("click", function() { 
-    if (video.requestFullscreen) {
-        video.requestFullscreen();
-    } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen();
+    if (isFullScreen()) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }      
+    } else {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        }
     }
 });
 
@@ -100,6 +118,7 @@ video.addEventListener('loadedmetadata', function() {
 
 
 video.addEventListener('progress', function() {
+        var currentBuffer = video.buffered.end(0);
         bufferBar.style.width = Math.floor((video.buffered.end(1-video.buffered.length)/video.duration)*100) + "%";
 });
 
@@ -108,70 +127,70 @@ video.addEventListener("timeupdate", function() {
     duration.innerHTML = currentDuration() + " / " 
                        + videoDuration();
     switch(currentDuration()) {
-        case "00:01": 
-            cap1.style.color = "orange";
+        case "00:00": 
+            cap[0].style.color = "orange";
             break;
         case "00:04":
-            cap1.style.color = "black";
-            cap2.style.color = "orange";
+            cap[0].style.color = "black";
+            cap[1].style.color = "orange";
             break;
-        case "00:07":
-            cap2.style.color = "black";
-            cap3.style.color = "orange";
+        case "00:08":
+            cap[1].style.color = "black";
+            cap[2].style.color = "orange";
             break;
         case "00:11":
-            cap3.style.color = "black";
-            cap4.style.color = "orange";
+            cap[2].style.color = "black";
+            cap[3].style.color = "orange";
             break;
         case "00:14":
-            cap4.style.color = "black";
-            cap5.style.color = "orange";
+            cap[3].style.color = "black";
+            cap[4].style.color = "orange";
             break;
         case "00:18":
-            cap5.style.color = "black";
-            cap6.style.color = "orange";
+            cap[4].style.color = "black";
+            cap[5].style.color = "orange";
             break;
         case "00:22":
-            cap6.style.color = "black";
-            cap7.style.color = "orange";
+            cap[5].style.color = "black";
+            cap[6].style.color = "orange";
             break;
         case "00:26":
-            cap7.style.color = "black";
-            cap8.style.color = "orange";
+            cap[6].style.color = "black";
+            cap[7].style.color = "orange";
             break;
         case "00:32":
-            cap8.style.color = "black";
-            cap9.style.color = "orange";
+            cap[7].style.color = "black";
+            cap[8].style.color = "orange";
             break;
         case "00:35":
-            cap9.style.color = "black";
-            cap10.style.color = "orange";
+            cap[8].style.color = "black";
+            cap[9].style.color = "orange";
             break;
         case "00:40":
-            cap10.style.color = "black";
-            cap11.style.color = "orange";
+            cap[9].style.color = "black";
+            cap[10].style.color = "orange";
             break;
         case "00:42":
-            cap11.style.color = "black";
-            cap12.style.color = "orange";
+            cap[10].style.color = "black";
+            cap[11].style.color = "orange";
             break;
         case "00:46":
-            cap12.style.color = "black";
-            cap13.style.color = "orange";
+            cap[11].style.color = "black";
+            cap[12].style.color = "orange";
             break;
         case "00:49":
-            cap13.style.color = "black";
-            cap14.style.color = "orange";
+            cap[12].style.color = "black";
+            cap[13].style.color = "orange";
             break;
         case "00:54":
-            cap14.style.color = "black";
-            cap15.style.color = "orange";
+            cap[13].style.color = "black";
+            cap[14].style.color = "orange";
             break;
         case "00:58":
-            cap15.style.color = "black";
-            cap16.style.color = "orange";
+            cap[14].style.color = "black";
+            cap[15].style.color = "orange";
             break;
-        default: cap16.style.color = "black";
+        default: cap[15].style.color = "black";
     }
 });
 
