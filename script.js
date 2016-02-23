2951c97e05833be53956ef26bbd9a19301367fa9
JHuppi <jhuppi@hotmail.com>
2016-02-23 13:40:14 -0700

    subtitles

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ playButton.addEventListener("click", function() {
 //Duration Display
 video.addEventListener('loadedmetadata', function() {
     duration.innerHTML = "00:00 / " + videoDuration();
+    var track = document.createElement("track");
+    track.kind = "subtitles";
+    track.label = "English";
+    track.srclang = "en";
+    track.src = "video/captions.vtt";
+    video.appendChild(track);
 });
 
 //Volume Bar Interactivity
@@ -79,16 +85,8 @@ muteButton.addEventListener("click", function() {
 
 //Captions Button
 ccButton.addEventListener("click", function() {
-    var track = document.createElement("track");
-    track.kind = "subtitles";
-    track.label = "English";
-    track.srclang = "en";
-    track.src = "video/captions.vtt";
-    track.addEventListener("load", function() {
-        this.mode = "showing";
-        video.textTracks[0].mode = "showing";
-    });
-    video.appendChild(track);
+    this.mode = "showing";
+    video.textTracks[0].mode = "showing";
 });
 
 //FullScreen Button
